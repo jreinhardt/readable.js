bd5aa3fa704afe408992dad4a030a14fc3ad826e
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2012-12-13 23:43:18 +0100

    Synchronised with master and adapted demo
    Everything is much easier now.

--- readable.js
+++ readable.js
@@ -1,7 +1,6 @@
+//Prototypes, utilities and the Text Object
 
-var readable = (function() {
-
-	var module = {};
+var readable = (function(module) {
 
 	/*
 	Count syllables in a word.
@@ -78,6 +77,121 @@ var readable = (function() {
 		return true;
 	}
 
+	/*
+	Prototype for metrics
+
+	Here common methods and properties can be collected
+	*/
+
+	module.MetricPrototype = {
+		getId: function(){throw "Method getId not implemented";},
+		getName: function(){throw "Method getName not implemented";},
+		getDescription: function(){throw "Method getDescription not implemented in " + this.getName();},
+		getReference: function(){throw "Method getReference not implemented in " + this.getName();},
+		getValue: function(){throw "Method getValue not implemented in " + this.getName();}
+	};
+
+	/*
+	Prototype for grade metrics
+
+	Grade metrics are a special case of metrics, as they return their estimate
+	in the form of a number indicating a grade in the US system.
+	*/
+
+	function GradeMetricPrototype(){
+		//source: http://en.wikipedia.org/wiki/Education_in_the_United_States#School_grades
+		this.getGrade = function(text){
+			var grade = this.getValue(text);
+			if(grade < -1){
+				return "Preschool";
+			} else if(grade <= 0){
+				return "Pre-Kindergarten";
+			} else if(grade <= 1){
+				return "Kindergarten";
+			} else if(grade <= 2){
+				return "1st Grade";
+			} else if(grade <= 3){
+				return "2nd Grade";
+			} else if(grade <= 4){
+				return "3rd Grade";
+			} else if(grade <= 5){
+				return "4th Grade";
+			} else if(grade <= 6){
+				return "5th Grade";
+			} else if(grade <= 7){
+				return "6th Grade";
+			} else if(grade <= 8){
+				return "7th Grade";
+			} else if(grade <= 9){
+				return "8th Grade";
+			} else if(grade <= 10){
+				return "9th Grade (Freshman)";
+			} else if(grade <= 11){
+				return "10th Grade (Sophomore)";
+			} else if(grade <= 12){
+				return "11th Grade (Junior)";
+			} else if(grade <= 13){
+				return "12th Grade (Senior)";
+			} else {
+				return "Tertiary education";
+			}
+		};
+
+		this.getRoughGrade = function(text){
+			var grade = this.getValue(text);
+			if(grade < 1){
+				return "Preschool";
+			} else if(grade <= 5){
+				return "Elementary school";
+			} else if(grade <= 8){
+				return "Middle school";
+			} else if(grade <= 12){
+				return "High school";
+			} else {
+				return "Tertiary education";
+			}
+		};
+	}
+	GradeMetricPrototype.prototype = module.MetricPrototype;
+
+	module.GradeMetricPrototype = new GradeMetricPrototype();
+
+
+	function isDecendant(object, prototype){
+		//Go up the prototype chain until a match is found or it ends
+		var proto = Object.getPrototypeOf(object);
+		while(proto !== prototype && proto instanceof Object){
+			proto = Object.getPrototypeOf(proto);
+		}
+		return proto === prototype;
+	}
+
+	/*
+	Obtain an array with selections of Metrics
+	*/
+
+	module.getAllMetrics = function(){
+		var metrics = [];
+		for(var attr in this){
+			//Slightly hacky, but I think it is reasonable to assume that Prototypes have Prototype in their name.
+			if(isDecendant(this[attr],module.MetricPrototype) && attr.search("Prototype") < 0){
+				metrics.push(this[attr]);
+			}
+		}
+		return metrics;
+	};
+
+	module.getAllGradeMetrics = function(){
+		var metrics = [];
+		for(var attr in this){
+			//Slightly hacky, but I think it is reasonable to assume that Prototypes have Prototype in their name.
+			if(isDecendant(this[attr],module.GradeMetricPrototype) && attr.search("Prototype") < 0){
+				metrics.push(this[attr]);
+			}
+		}
+		return metrics;
+	};
+
 
 	/*
 	Factory for Text objects
@@ -102,77 +216,6 @@ var readable = (function() {
 			getNumPolysyllables: function(){return memoize("numPolysyllables",countPolysyllables);},
 			getNumSentences: function(){return memoize("numSentences",countSentences);},
 
-			/*
-			Compute the Flesch readability index.
-
-			Returns the Flesch readability index, a number (usually) between 0
-			and 100, where larger numbers indicate easier text
-
-			https://en.wikipedia.org/wiki/Flesch-Kincaid_Readability_Test
-			*/
-			getFleschKincaidIndex: function() {
-				var wps = this.getNumWords() / this.getNumSentences();
-				var spw = this.getNumSyllables() / this.getNumWords();
-				return 206.835 - 1.015 * wps - 84.6 * spw;
-			},
-
-			/*
-			Compute the Coleman-Liau readability index.
-
-			This functions returns the Coleman-Liau readability index indicating
-			the number of years of education necessary to understand the text.
-
-			https://en.wikipedia.org/wiki/Coleman-Liau_Index
-			 */
-			getColemanLiauIndex: function() {
-				var lpw = 100*this.getNumLetters()/this.getNumWords();
-				var spw = 100*this.getNumSentences()/this.getNumWords();
-				return 0.0588 * lpw - 0.296 * spw - 15.8;
-			},
-
-
-			/*
-			Compute the Automated Readability Index.
-
-			This function returns the Automated Readability Index indicating
-			the number of years of education necessary to understand the text.
-
-			https://en.wikipedia.org/wiki/Automated_Readability_Index
-			 */
-			getAutomatedReadabilityIndex: function() {
-				var lpw = this.getNumLetters()/this.getNumWords();
-				var wps = this.getNumWords() / this.getNumSentences();
-				return 4.71 * lpw + 0.5 * wps - 21.43;
-			},
-
-
-			/*
-			Compute the Gunning-Fog Index
-
-			This function returns the Gunning-Fog index indicating
-			the number of years of education necessary to understand the text.
-
-			https://en.wikipedia.org/wiki/Gunning_fog_index
-			*/
-			getGunningFogIndex: function(){
-				var wps = this.getNumWords() / this.getNumSentences();
-				var ppw = this.getNumPolysyllables() / this.getNumWords();
-				return 0.4*(wps + 100.*ppw);
-			},
-
-			/*
-			Compute the SMOG grade
-
-			This function returns the SMOG indicating the number of years of
-			education necessary to understand the text. It is slightly different
-			from the SMOG Index.
-
-			https://en.wikipedia.org/wiki/SMOG
-			*/
-			getSmogGrade: function(ns,nc){
-				var pps = this.getNumPolysyllables() / this.getNumSentences();
-				return 1.043*Math.sqrt(30*pps) + 3.1291;
-			}
 		}
 
 
@@ -286,4 +329,4 @@ var readable = (function() {
 	}
 
 	return module;
-}());
+}(readable || {}));
